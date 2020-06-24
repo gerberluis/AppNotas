2cbe6447dd8a7f51ecbc9fd44a4f703b8c959433
gerberluis <67350870+gerberluis@users.noreply.github.com>
2020-06-23 22:05:58 -0500

    Proyecto actualizado 23062020

--- AppGitNotas/AppGitNotas/MainPage.xaml.cs
+++ AppGitNotas/AppGitNotas/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using System.IO;
 
 namespace AppGitNotas
 {
@@ -13,9 +14,46 @@ namespace AppGitNotas
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
+        string archivo = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "notas.txt");
         public MainPage()
         {
             InitializeComponent();
+            if (File.Exists(archivo))
+            {
+                txtEditor.Text = File.ReadAllText(archivo);
+            }
+        }
+
+        private void btnGrabar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllText(archivo, txtEditor.Text);
+                DisplayAlert("Exito", "Archivo Grabado con Exito", "OK");
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error",ex.Message, "OK");
+            }
+           
+        }
+
+        private void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            if (File.Exists(archivo))
+            {
+                try
+                {
+                    File.Delete(archivo);
+                    DisplayAlert("Exito", "Archivo Eliminado con Exito", "OK");
+                }
+                catch(Exception ex)
+                {
+                    DisplayAlert("Error", ex.Message, "OK");
+                }
+               
+            }
+            txtEditor.Text = string.Empty;
         }
     }
 }
